f320211bce58ba957ee4f272f9bb0cc0ac2f3cfa
Roopa-Sundar <rupasundar9849@gmail.com>
2024-06-03 12:58:45 +0530

    fix

--- config/index.ts
+++ config/index.ts
@@ -1,7 +1,7 @@
 import type { Metadata } from "next";
 
 export const siteConfig: Metadata = {
-  title: "Roopa Sundar | Portfolio",
+  title: "Roopa Sundar ",
   description: "Welcome to my full stack Next.js 14 space portfolio.",
   keywords: [
     "reactjs",
@@ -35,7 +35,7 @@ export const siteConfig: Metadata = {
     "css",
   ] as Array<string>,
   authors: {
-    name: "Sanidhya Kumar Verma",
+    name: "Roopa Sundar",
     url: "",
   },
 } as const;
